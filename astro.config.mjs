54fbc1ca5c3d121cd450ecd57c1d59ba1dd8362a
Bladimir Almanzar <bladdy34@gmail.com>
2025-06-20 12:06:08 -0600

    feat: agregar configuración del devToolbar y cambiar el idioma de la página a español

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,6 +24,11 @@ export default defineConfig({
   server: {
     host: true,
     port: 4321,
+  },
+  devToolbar: {
+    // Disable the developer toolbar
+    enabled: false,
+    // You can also modify other options here
   }
 
 });
