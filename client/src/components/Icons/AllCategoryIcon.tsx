5028ead13a8005f9460f8731dd0b59b526426e27
Bladimir Almanzar <bladdy34@gmail.com>
2025-07-21 16:15:42 -0600

    changed: Allcategory icon

--- client/src/components/Icons/AllCategoryIcon.tsx
+++ client/src/components/Icons/AllCategoryIcon.tsx
@@ -17,8 +17,7 @@ const AllCategoryIcon: React.FC<Props> = ({ className }) => {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-    > <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4h6v6h-6zm10 0h6v6h-6zm-10 10h6v6h-6zm10 3h6" /></svg>
+    > <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4h6v6h-6zm10 0h6v6h-6zm-10 10h6v6h-6zm10 3h6m-3 -3v6" /></svg>
   );
 };
-
 export default AllCategoryIcon;
