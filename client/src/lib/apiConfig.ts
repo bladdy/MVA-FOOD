d89645f2fa0d5d6d0301b94495c4354e67918135
Bladimir Almanzar <bladdy34@gmail.com>
2026-06-27 01:00:24 -0600

    feat: Add PUBLIC_HUB_URL to environment configuration and update apiConfig

--- client/src/lib/apiConfig.ts
+++ client/src/lib/apiConfig.ts
@@ -1,2 +1,3 @@
 // client/src/lib/apiConfig.ts
 export const API_URL = import.meta.env.PUBLIC_API_URL || "https://api.mr-menus.com/api";
+export const HUB_URL =  import.meta.env.PUBLIC_HUB_URL ||"https://api.mr-menus.com/hubs/orders";
